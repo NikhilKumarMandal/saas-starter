6f1d940d22c4c8fdc88382e8e4b69d4c146fdb13
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-10-16 23:54:25 +0530

    Update .env with Prisma and Clerk configuration

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,49 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { authMiddleware, clerkClient } from '@clerk/nextjs/server'
+import { NextRequest,NextResponse } from 'next/server'
 
-export default clerkMiddleware()
+const publicRoutes = [
+  "/",
+  "/api/webhook/register",
+  "sign-up",
+  "sign-in"
+]
+
+export default authMiddleware({
+  publicRoutes,
+  async afterAuth(auth,req:NextRequest) {
+    // handle unauth user trying to access protected routes
+    if (!auth.userId && !publicRoutes.includes(req.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL("sign-in",req.url))
+    }
+
+    if (auth.userId) {
+     try {
+       const user = await clerkClient.users.getUser(auth.userId)
+       const role = user.publicMetadata.role as string | undefined;
+ 
+       if (role === "admin" && req.nextUrl.pathname === "/dashboard") {
+         return NextResponse.redirect(new URL("/admin/dashboard",req.url))
+       }
+ 
+       if (role !== "admin" && req.nextUrl.pathname.startsWith("/admin")) {
+       return NextResponse.redirect(new URL("/dashboard",req.url))
+       }
+ 
+       if (publicRoutes.includes(req.nextUrl.pathname)) {
+         return NextResponse.redirect(
+           new URL(
+           role === "admin" ? "/admin/dashboard" : "/dashboard",
+           req.url
+           )
+         )
+       }
+     } catch (error) {
+       console.error(error)
+       return NextResponse.redirect(new URL("/error",req.url))
+     }
+    }
+  }
+})
 
 export const config = {
   matcher: [
